13d56a6098817be3667b6abfc71f60e8b0261e17
udarieimalsha <udarieimalsha@gmail.com>
2026-03-25 14:21:20 +0530

    Release v1.0.9 with fully seamless update flow

--- Installer/BuildInstaller.ps1
+++ Installer/BuildInstaller.ps1
@@ -2,7 +2,7 @@
 # This script will download Inno Setup if missing and compile the installer.
 
 $issPath = ".\RiyanSetup.iss"
-$outputExe = "..\RiyanSetup_v1.0.8.exe"
+$outputExe = "..\RiyanSetup_v1.0.9.exe"
 
 Write-Host "--- Riyan Revit Tools Installer Builder ---" -ForegroundColor Cyan
 
